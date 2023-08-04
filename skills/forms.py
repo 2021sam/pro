28dda85fd375ddb006110792bb53acc6e3171207
2021sam <7050592@gmail.com>
2023-08-03 19:54:38 -0700

    Add Dependent Skills on Experience

--- skills/forms.py
+++ skills/forms.py
@@ -1,7 +1,16 @@
-from django.forms import ModelForm
+from django import forms
+# from django.forms import ModelForm
 from .models import Skills
+from experience.models import Experience
 
-class SkillsForm(ModelForm):
+class SkillsForm(forms.ModelForm):
     class Meta:
         model = Skills
-        fields = ['experience', 'skill', 'skill_years', 'skill_months']
+        fields = ['skill', 'skill_years', 'skill_months']
+
+    def __init__(self, *args, **kwargs):
+            self.user = kwargs.pop('user', None)
+            if self.user:
+                print(self.user.id)
+            super(SkillsForm, self).__init__(*args, **kwargs)
+            self.fields['experience'] = forms.ModelMultipleChoiceField(queryset=Experience.objects.filter(author=self.user.id))
--- skills/views.py
+++ skills/views.py
@@ -6,6 +6,8 @@ from django.http import HttpResponse
 from .models import Skills
 from .forms import SkillsForm
 from django.urls import path
+from .models import Experience
+
 
 @login_required
 def home(request):
@@ -23,22 +25,33 @@ def view(request):
 @login_required
 def add(request):
     if request.method == 'GET':
-        context = {'form': SkillsForm()}
-        return render(request,'skills/add_edit.html',context)
+        # experience = Experience.objects.filter(author=request.user)
+        # context = {'form': SkillsForm(user=request.user),
+        #            'experience': experience}
+        # experience = Experience.objects.filter(author=request.user)
+        context = {'form': SkillsForm(user=request.user)}
+        return render(request,'skills/add_edit.html', context)
+
     elif request.method == 'POST':
-        form = SkillsForm(request.POST)
+        form = SkillsForm(request.POST, user=request.user)
         if form.is_valid():
-            i = form.save(commit=False)
-            i.author = request.user
-            i.save()
+            experience_id = request.POST.get('experience')
+            skill = request.POST.get('skill')
+            skill_years = request.POST.get('skill_years')
+            skill_months = request.POST.get('skill_months')
+            e = Experience.objects.get(id=experience_id)
+            i = Skills.objects.create(author=request.user, experience=e, skill=skill, skill_years=skill_years, skill_months=skill_months)
+            print(i)
+            i.save()    # For some reason it saves with out this line
             messages.success(request, 'The post has been successfully created.')
             return redirect('skills-view')
+        
         else:
             messages.error(request, 'Please correct the following errors:')
-            return render(request,'skills/add_edit.html', {'form':form})  
+            return render(request,'skills/add_edit.html', {'form':form})
 
 
-@login_required    
+@login_required
 def edit(request, id):
     queryset = Skills.objects.filter(author=request.user)
     skills = get_object_or_404(queryset, pk=id)
@@ -63,7 +76,7 @@ def delete(request, id):
     queryset = Skills.objects.filter(author=request.user)
     skills = get_object_or_404(queryset, pk=id)
     context = {'skills': skills}
-    
+
     if request.method == 'GET':
         return render(request, 'skills/delete.html', context)
     elif request.method == 'POST':
