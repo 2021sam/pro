3e23758c1db1ad30c883f8afb684eaf65c55cb38
2021sam <7050592@gmail.com>
2023-08-12 20:43:34 -0700

    Now working

--- skill/forms.py
+++ skill/forms.py
@@ -7,7 +7,7 @@ from django.forms import BaseModelFormSet
 class SkillForm(forms.ModelForm):
     class Meta:
         model = Skill
-        fields = ['skill', 'skill_years', 'skill_months']
+        fields = ['experience', 'skill', 'skill_years', 'skill_months']
 
 
     def __init__(self, *args, **kwargs):
@@ -15,15 +15,11 @@ class SkillForm(forms.ModelForm):
             if self.user:
                 print('SkillForm __init__:')
                 print(self.user.id)
-            super(SkillForm, self).__init__(*args, **kwargs)
-            self.fields['experience'] = forms.ModelMultipleChoiceField(queryset=Experience.objects.filter(user=self.user.id))
-
-            # widgets={
-            #             'experience': forms.Select(attrs={'size': '120px'})
-            #             }
-            # widgets={
-            #             'experience': forms.Select(attrs={'style': 'width:120px'})
-            #             }
+                super(SkillForm, self).__init__(*args, **kwargs)
+                mystyle = {"style": "width:300px;", "size": 1, "rows": 10} # rows does not seem to have an affect
+                self.fields['experience'] = forms.ModelChoiceField(queryset=Experience.objects.filter(user=self.user.id), widget=forms.Select(), required=False)
+                self.fields["experience"].widget.attrs = mystyle
+                self.fields["skill"].widget.attrs = {"style": "width:300px;"}
 
 # class BaseSkillFormSet(BaseModelFormSet):
 #      def __init__(self, *args, **kwargs):
--- skill/views.py
+++ skill/views.py
@@ -15,22 +15,20 @@ from django.views.decorators.http import require_http_methods
 
 
 def edit(request):
-    # SkillModelFormSet = modelformset_factory(Skill, fields=['id', 'experience', 'skill', 'skill_years', 'skill_months'], extra=3, can_delete=True)
-    SkillModelFormSet = modelformset_factory(Skill, form=SkillForm, extra=0, can_delete=True)
+    SkillModelFormSet = modelformset_factory(Skill, form=SkillForm, extra=3, can_delete=True)
     if request.method == 'GET':
         queryset = Skill.objects.filter(user=request.user)
         print(queryset.count)
         print(len(queryset))
-        print(queryset[0].experience)
+        # print(queryset[0].experience)
 
         # https://stackoverflow.com/questions/622982/django-passing-custom-form-parameters-to-formset
         skillformset = SkillModelFormSet(form_kwargs={'user': request.user}, queryset = Skill.objects.filter(user=request.user))
         context = {'formset': skillformset}
-        # return render(request, 'skill/edit_set.html', context)
         return render(request, 'skill/edit_set_row.html', context)
 
     elif request.method == 'POST':
-        form = SkillModelFormSet(request.POST, request.FILES)
+        form = SkillModelFormSet(request.POST, request.FILES, form_kwargs={'user': request.user})
         print(f'form.is_bound1 = {form.is_bound}')
         print(f'form.has_changed() = {form.has_changed()}')
         print(f'form.is_valid() = {form.is_valid()}')
@@ -107,86 +105,85 @@ def add_skills(d, skill, years, months):
 
 
 
-@login_required
-def add(request):
-    if request.method == 'GET':
-        # experience = Experience.objects.filter(author=request.user)
-        # context = {'form': SkillsForm(user=request.user),
-        #            'experience': experience}
-        # experience = Experience.objects.filter(author=request.user)
-        context = {'form': SkillForm(user=request.user)}
-        return render(request,'skill/add_edit.html', context)
+# @login_required
+# def add(request):
+#     if request.method == 'GET':
+#         # experience = Experience.objects.filter(author=request.user)
+#         # context = {'form': SkillsForm(user=request.user),
+#         #            'experience': experience}
+#         # experience = Experience.objects.filter(author=request.user)
+#         context = {'form': SkillForm(user=request.user)}
+#         return render(request,'skill/add_edit.html', context)
 
-    elif request.method == 'POST':
-        form = SkillForm(request.POST, user=request.user)
-        if form.is_valid():
-            experience_id = request.POST.get('experience')
-            skill = request.POST.get('skill')
-            skill_years = request.POST.get('skill_years')
-            skill_months = request.POST.get('skill_months')
-            e = Experience.objects.get(id=experience_id)
-            i = Skill.objects.create(user=request.user, experience=e, skill=skill, skill_years=skill_years, skill_months=skill_months)
-            print(i)
-            i.save()    # For some reason it saves with out this line
-            messages.success(request, 'The post has been successfully created.')
-            return redirect('skill:skill-view')
+#     elif request.method == 'POST':
+#         form = SkillForm(request.POST, user=request.user)
+#         if form.is_valid():
+#             experience_id = request.POST.get('experience')
+#             skill = request.POST.get('skill')
+#             skill_years = request.POST.get('skill_years')
+#             skill_months = request.POST.get('skill_months')
+#             e = Experience.objects.get(id=experience_id)
+#             i = Skill.objects.create(user=request.user, experience=e, skill=skill, skill_years=skill_years, skill_months=skill_months)
+#             print(i)
+#             i.save()    # For some reason it saves with out this line
+#             messages.success(request, 'The post has been successfully created.')
+#             return redirect('skill:skill-view')
         
-        else:
-            messages.error(request, 'Please correct the following errors:')
-            return render(request,'skill/add_edit.html', {'form':form})
+#         else:
+#             messages.error(request, 'Please correct the following errors:')
+#             return render(request,'skill/add_edit.html', {'form':form})
 
 
-@login_required
-def edit0(request, id):
-    queryset = Skill.objects.filter(user=request.user)
-    skills = get_object_or_404(queryset, pk=id)
+# @login_required
+# def edit0(request, id):
+#     queryset = Skill.objects.filter(user=request.user)
+#     skills = get_object_or_404(queryset, pk=id)
 
-    if request.method == 'GET':
-        context = {'form': SkillForm(instance=skills, user=request.user), 'id': id}
-        return render(request,'skill/add_edit.html', context)
+#     if request.method == 'GET':
+#         context = {'form': SkillForm(instance=skills, user=request.user), 'id': id}
+#         return render(request,'skill/add_edit.html', context)
     
-    elif request.method == 'POST':
-        form = SkillForm(request.POST, user=request.user, instance=skills)
-        if form.is_valid():
-            experience_id = request.POST.get('experience')
-            skill = request.POST.get('skill')
-            skill_years = request.POST.get('skill_years')
-            skill_months = request.POST.get('skill_months')
-            e = Experience.objects.get(id=experience_id)
-            i = Skill.objects.get(id=id)
-            i.experience = e
-            i.skill = skill
-            i.skill_years = skill_years
-            i.skill_months = skill_months
-            print(i)
-            i.save()
-            messages.success(request, 'The post has been updated successfully.')
-            return redirect('skill:skill-view')
+#     elif request.method == 'POST':
+#         form = SkillForm(request.POST, user=request.user, instance=skills)
+#         if form.is_valid():
+#             experience_id = request.POST.get('experience')
+#             skill = request.POST.get('skill')
+#             skill_years = request.POST.get('skill_years')
+#             skill_months = request.POST.get('skill_months')
+#             e = Experience.objects.get(id=experience_id)
+#             i = Skill.objects.get(id=id)
+#             i.experience = e
+#             i.skill = skill
+#             i.skill_years = skill_years
+#             i.skill_months = skill_months
+#             print(i)
+#             i.save()
+#             messages.success(request, 'The post has been updated successfully.')
+#             return redirect('skill:skill-view')
 
-            # SkillModelFormSet = modelformset_factory(Skill, fields=['skill', 'skill_years', 'skill_months'])
-            # skillformset = SkillModelFormSet(queryset = Skill.objects.filter(user=request.user))
-            # context = {'formset': skillformset}
-            # return render(request, 'skill/viewset.html', context)
-
-
-
-        else:
-            messages.error(request, 'Please correct the following errors:')
-            return render(request,'skill/add_edit.html', {'form':form})
+#             # SkillModelFormSet = modelformset_factory(Skill, fields=['skill', 'skill_years', 'skill_months'])
+#             # skillformset = SkillModelFormSet(queryset = Skill.objects.filter(user=request.user))
+#             # context = {'formset': skillformset}
+#             # return render(request, 'skill/viewset.html', context)
 
 
-@login_required
-def delete(request, id):
-    queryset = Skill.objects.filter(user=request.user)
-    skills = get_object_or_404(queryset, pk=id)
-    context = {'skills': skills}
+#         else:
+#             messages.error(request, 'Please correct the following errors:')
+#             return render(request,'skill/add_edit.html', {'form':form})
 
-    if request.method == 'GET':
-        return render(request, 'skill/delete.html', context)
-    elif request.method == 'POST':
-        skills.delete()
-        messages.success(request,  'The post has been deleted successfully.')
-        return redirect('skill:skill-view')
+
+# @login_required
+# def delete(request, id):
+#     queryset = Skill.objects.filter(user=request.user)
+#     skills = get_object_or_404(queryset, pk=id)
+#     context = {'skills': skills}
+
+#     if request.method == 'GET':
+#         return render(request, 'skill/delete.html', context)
+#     elif request.method == 'POST':
+#         skills.delete()
+#         messages.success(request,  'The post has been deleted successfully.')
+#         return redirect('skill:skill-view')
 
 
 def hx_delete(request, id):
