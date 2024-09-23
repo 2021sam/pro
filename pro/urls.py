4fc2320c776df9ba3f96f688eed31ae68d4d857f
2021sam <2021sam@2018mac.local>
2024-09-23 14:19:59 -0700

    trailhead is the new home page

--- pro/urls.py
+++ pro/urls.py
@@ -6,13 +6,10 @@ from authenticate.views import home, custom_login, custom_logout
 from authenticate import views as user_views
 
 urlpatterns = [
-    path('', home, name='home'),  # Root URL points to the home view
     path('admin/', admin.site.urls),
     path('authenticate/', include('authenticate.urls')),  # Include user app URLs
     path('accounts/login/', custom_login, name='login'),  # Override the default login view
     path('accounts/', include('django.contrib.auth.urls')),  # Include Django's built-in authentication views for the other routes
-        # Logout URL
-    # path('logout/', auth_views.LogoutView.as_view(), name='logout'),
-    # path('logout/', auth_views.LogoutView.as_view(next_page='home'), name='logout'),
     path('logout/', custom_logout, name='logout'),
+    path('', include('trailhead.urls'))
 ]
