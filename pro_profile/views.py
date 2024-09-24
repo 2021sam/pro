cc90d9ea1113f9033034987c041c21b7867ad28b
2021sam <2021sam@2018mac.local>
2024-09-23 22:13:42 -0700

    attempt 2

--- pro_profile/views.py
+++ pro_profile/views.py
@@ -7,37 +7,65 @@ from .forms import ProfileForm
 from django.contrib.auth.models import User
 
 
-# Create your views here.
-# http://localhost:8000/profile/edit/1
-# def profile_edit(request, id):
+# @login_required
+# def profile_edit(request):
+#     profile, created = Profile.objects.get_or_create(user=request.user)
 
+#     if created:
+#         print("Profile created for the user.")
+    
+#     form = ProfileForm(instance=profile)
+#     queryset = Profile.objects.filter(user=request.user)
+#     print(queryset)
+#     for e in queryset:
+#         print(e.id)
+#         print(e.user)
+#         id = e.id
+#         current_user = e.user
+#         print(f'id = {id}')
+#         print(f'user={current_user}')
+
+#     form = get_object_or_404(queryset, pk=id)
+
+#     if request.method == 'GET':
+#         context = {'form': ProfileForm(instance=form), 'id': id}
+#         return render(request,'pro_profile/profile_form.html',context)
+
+#     elif request.method == 'POST':
+#         form = ProfileForm(request.POST, instance=form)
+#         if form.is_valid():
+#             form.save()
+#             messages.success(request, 'The post has been updated successfully.')
+#             return redirect('home')
+#         else:
+#             messages.error(request, 'Please correct the following errors:')
+#             return render(request,'pro_profile/profile_form.html',{'form':form})
+
+
+
+#   chat suggests this version
 @login_required
 def profile_edit(request):
-    queryset = Profile.objects.filter(user=request.user)
-    print(queryset)
-    for e in queryset:
-        print(e.id)
-        print(e.user)
-        id = e.id
-        current_user = e.user
-        print(f'id = {id}')
-        print(f'user={current_user}')
+    profile, created = Profile.objects.get_or_create(user=request.user)
 
-    form = get_object_or_404(queryset, pk=id)
-
-    if request.method == 'GET':
-        context = {'form': ProfileForm(instance=form), 'id': id}
-        return render(request,'pro_profile/profile_form.html',context)
-
-    elif request.method == 'POST':
-        form = ProfileForm(request.POST, instance=form)
+    if created:
+        print("Profile created for the user.")
+    
+    if request.method == 'POST':
+        form = ProfileForm(request.POST, instance=profile)
         if form.is_valid():
             form.save()
-            messages.success(request, 'The post has been updated successfully.')
-            return redirect('trailhead')
+            messages.success(request, 'Your profile has been updated successfully.')
+            return redirect('home')  # Redirect to an appropriate page after saving
         else:
-            messages.error(request, 'Please correct the following errors:')
-            return render(request,'pro_profile/profile_form.html',{'form':form})
+            messages.error(request, 'Please correct the errors below.')
+    else:
+        form = ProfileForm(instance=profile)
+
+    context = {'form': form, 'id': profile.id}
+    return render(request, 'pro_profile/profile_form.html', context)
+
+
 
 
 def tool_profile_mia(request):
