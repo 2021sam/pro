acd4b55184e03f49869b44c7e1b466c7f92aadcd
2021sam <2021sam@2018mac.local>
2024-09-24 01:59:21 -0700

    attempt 3

--- pro_profile/forms.py
+++ pro_profile/forms.py
@@ -1,3 +1,22 @@
+from django import forms
+from django.forms import ModelForm
+from .models import Profile
+
+class ProfileForm(ModelForm):
+    class Meta:
+        model = Profile
+        fields = ['user', 'work_authorization', 'willing_to_relocate', 'address', 'birth_month', 'birth_day', 'birth_date', 'drivers_license', 'linkedin', 'open_to_public']
+
+    def __init__(self, *args, **kwargs):
+        super(ProfileForm, self).__init__(*args, **kwargs)
+        self.fields['user'].widget.attrs['readonly'] = True  # Make user field read-only
+
+
+
+
+
+
+
 # from django import forms
 # from .models import Profile
 # from django.contrib.auth import get_user_model
@@ -13,14 +32,14 @@
 
 
 
-from django import forms
-from django.forms import ModelForm
-from .models import Profile
+# from django import forms
+# from django.forms import ModelForm
+# from .models import Profile
 
-class ProfileForm(ModelForm):
-    class Meta:
-        model = Profile
-        fields = ['user', 'work_authorization', 'willing_to_relocate', 'address', 'birth_month', 'birth_day', 'birth_date', 'drivers_license', 'linkedin', 'open_to_public']
+# class ProfileForm(ModelForm):
+#     class Meta:
+#         model = Profile
+#         fields = ['user', 'work_authorization', 'willing_to_relocate', 'address', 'birth_month', 'birth_day', 'birth_date', 'drivers_license', 'linkedin', 'open_to_public']
 
         # widgets = {
         #     'user': forms.EmailInput(attrs={'readonly': 'readonly'}),
