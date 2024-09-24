8fd6258e3bcb7c1f9b45fd4ac0ad1eebf49c47ac
2021sam <2021sam@2018mac.local>
2024-09-24 02:11:37 -0700

    success

--- pro_profile/forms.py
+++ pro_profile/forms.py
@@ -2,6 +2,7 @@ from django import forms
 from django.forms import ModelForm
 from .models import Profile
 
+
 class ProfileForm(ModelForm):
     class Meta:
         model = Profile
@@ -9,38 +10,5 @@ class ProfileForm(ModelForm):
 
     def __init__(self, *args, **kwargs):
         super(ProfileForm, self).__init__(*args, **kwargs)
-        self.fields['user'].widget.attrs['readonly'] = True  # Make user field read-only
-
-
-
-
-
-
-
-# from django import forms
-# from .models import Profile
-# from django.contrib.auth import get_user_model
-
-# class ProfileForm(forms.ModelForm):
-#     class Meta:
-#         model = Profile
-#         fields = ['address', 'birth_date', 'mobile_number', 'mobile_carrier']  # Add other editable fields
-
-#     def __init__(self, *args, **kwargs):
-#         super(ProfileForm, self).__init__(*args, **kwargs)
-#         self.fields['user'].widget.attrs['readonly'] = True
-
-
-
-# from django import forms
-# from django.forms import ModelForm
-# from .models import Profile
-
-# class ProfileForm(ModelForm):
-#     class Meta:
-#         model = Profile
-#         fields = ['user', 'work_authorization', 'willing_to_relocate', 'address', 'birth_month', 'birth_day', 'birth_date', 'drivers_license', 'linkedin', 'open_to_public']
-
-        # widgets = {
-        #     'user': forms.EmailInput(attrs={'readonly': 'readonly'}),
-        # }
+        # Make the user field non-editable by disabling it
+        self.fields['user'].disabled = True
