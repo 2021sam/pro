267ac16329d815f6069522926f5d2dea87aad442
2021sam <2021sam@2018mac.local>
2024-10-04 18:52:22 -0700

    attempt 4

--- employer_skill/views.py
+++ employer_skill/views.py
@@ -9,6 +9,45 @@ def skill_list(request):
     skills = EmployerSkill.objects.all()
     return render(request, 'employer_skill/skill_list.html', {'skills': skills})
 
+
+
+from django.shortcuts import render, redirect, get_object_or_404
+from .models import EmployerSkill
+from .forms import EmployerSkillForm, EmployerSkillFormSet
+from django.forms import modelformset_factory
+from employer_job.models import EmployerJob
+
+def skill_add(request, job_id):
+    # Get the job based on the job_id from the URL
+    job = get_object_or_404(EmployerJob, id=job_id)
+
+    # Use modelformset_factory to create a formset for the EmployerSkill model
+    SkillFormSet = modelformset_factory(EmployerSkill, form=EmployerSkillForm, extra=1)
+
+    if request.method == 'POST':
+        formset = SkillFormSet(request.POST)
+        if formset.is_valid():
+            instances = formset.save(commit=False)
+            for instance in instances:
+                instance.user = request.user  # Assign the current user
+                instance.job = job  # Assign the job from the URL
+                instance.save()
+            formset.save_m2m()  # Save any many-to-many relationships, if any
+            return redirect('employer_skill:emp_skill_list')
+        else:
+            print('Formset Errors:', formset.errors)
+    else:
+        # Ensure to set the job for each form in the formset
+        formset = SkillFormSet(queryset=EmployerSkill.objects.filter(job=job))
+
+    # Render the formset in the template for creating multiple skills, with the job already set
+    return render(request, 'employer_skill/skill_formset.html', {'formset': formset, 'job': job, 'form_type': 'create'})
+
+
+
+
+
+
 # def skill_add(request):
 #     # Use modelformset_factory to create a formset for Skill model
 #     SkillFormSet = modelformset_factory(EmployerSkill, form=EmployerSkillForm, extra=1)
@@ -38,30 +77,30 @@ from employer_job.models import EmployerJob
 # from django.forms import modelformset_factory
 
 
-def skill_add(request, job_id):
-    # Get the job based on the job_id from the URL
-    job = get_object_or_404(EmployerJob, id=job_id)
+# def skill_add(request, job_id):
+#     # Get the job based on the job_id from the URL
+#     job = get_object_or_404(EmployerJob, id=job_id)
 
-    # Use modelformset_factory to create a formset for the EmployerSkill model
-    SkillFormSet = modelformset_factory(EmployerSkill, form=EmployerSkillForm, extra=1)
+#     # Use modelformset_factory to create a formset for the EmployerSkill model
+#     SkillFormSet = modelformset_factory(EmployerSkill, form=EmployerSkillForm, extra=1)
 
-    if request.method == 'POST':
-        formset = SkillFormSet(request.POST)
-        if formset.is_valid():
-            instances = formset.save(commit=False)
-            for instance in instances:
-                instance.user = request.user  # Assign the current user
-                instance.job = job  # Assign the job passed in the URL
-                instance.save()
-            formset.save_m2m()  # Save any many-to-many relationships, if any
-            return redirect('employer_skill:emp_skill_list')
-        else:
-            print('Formset Errors:', formset.errors)
-    else:
-        formset = SkillFormSet(queryset=EmployerSkill.objects.none())  # Show an empty formset for new entries
+#     if request.method == 'POST':
+#         formset = SkillFormSet(request.POST)
+#         if formset.is_valid():
+#             instances = formset.save(commit=False)
+#             for instance in instances:
+#                 instance.user = request.user  # Assign the current user
+#                 instance.job = job  # Assign the job passed in the URL
+#                 instance.save()
+#             formset.save_m2m()  # Save any many-to-many relationships, if any
+#             return redirect('employer_skill:emp_skill_list')
+#         else:
+#             print('Formset Errors:', formset.errors)
+#     else:
+#         formset = SkillFormSet(queryset=EmployerSkill.objects.none())  # Show an empty formset for new entries
 
-    # Render the formset in the template for creating multiple skills, with the job already set
-    return render(request, 'employer_skill/skill_formset.html', {'formset': formset, 'job': job, 'form_type': 'create'})
+#     # Render the formset in the template for creating multiple skills, with the job already set
+#     return render(request, 'employer_skill/skill_formset.html', {'formset': formset, 'job': job, 'form_type': 'create'})
 
 
 
