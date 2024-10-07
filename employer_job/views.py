a882cae7b834018ff5b01937c477b5b315ff26f6
2021sam <2021sam@2018mac.local>
2024-10-06 23:06:13 -0700

    attempt 12

--- employer_job/views.py
+++ employer_job/views.py
@@ -32,6 +32,9 @@ from employer_skill.models import EmployerSkill  # Assuming EmployerSkill is the
 from .forms import EmployerJobForm
 from employer_skill.forms import EmployerSkillFormSet  # Make sure this is imported
 
+
+
+@login_required
 def add_edit_job_with_skills(request, job_id=None):
     # Fetch job if job_id is provided (edit case), otherwise create new (add case)
     if job_id:
@@ -44,23 +47,26 @@ def add_edit_job_with_skills(request, job_id=None):
         formset = EmployerSkillFormSet(queryset=EmployerSkill.objects.none())
 
     if request.method == 'POST':
+        # Pass the request.POST to the formset to include the hidden inputs
         job_form = EmployerJobForm(request.POST, instance=job)
-        formset = EmployerSkillFormSet(request.POST)
+        formset = EmployerSkillFormSet(request.POST)  # This will include hidden inputs
         print(f'formset: {formset}')
         # Validate both job form and formset
         if job_form.is_valid() and formset.is_valid():
             # Save the job
-            # Save the job and assign the user
             job = job_form.save(commit=False)
             job.user = request.user  # Set the current logged-in user
             job.save()
-
 
             # Save the skills, assigning the job to each skill in the formset
             skills = formset.save(commit=False)
             for skill in skills:
                 skill.job = job
                 skill.user = request.user  # Set the current logged-in user for each skill
+                
+                # Capture skill_months value
+                skill_months = request.POST.get(f'skill_months_{skill.id}')  # Adjust if necessary to match how your skill ID is formed
+                skill.skill_months = skill_months  # Assign the captured skill_months
                 skill.save()
 
             return redirect('employer_job:job-view')  # Adjust to your desired redirect URL
@@ -81,6 +87,7 @@ def add_edit_job_with_skills(request, job_id=None):
         'max_slider_value': max_slider_value_months,     # Pass the slider max value to the template
         'show_months': show_months  # Pass the toggle variable for months
     })
+
 
 
 
