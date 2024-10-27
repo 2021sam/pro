3ce8efb1dd50d9b561766da9933944072be8fc28
2021sam <2021sam@Zyxes-Mac-mini.local>
2024-10-27 13:46:08 -0700

    updated urls.py

--- employer_search/urls.py
+++ employer_search/urls.py
@@ -1,15 +1,16 @@
 # /Users/2021sam/apps/zyxe/pro/employer_search/urls.py
 
+
+# employer_search/urls.py
 from django.urls import path
 from . import views
 
 app_name = 'employer_search'
 
 urlpatterns = [
-    # URL for the search form where recruiters can input job zip code and search for freelancers
-    # path('search/', views.search_freelancers, name='search_freelancers'),
+    # Search for freelancers by job
     path('search/<int:job_id>/', views.search_freelancers_by_job, name='search_freelancers_by_job'),
 
-    # URL for viewing the search results
-    # path('results/', views.search_results, name='search_results'),
+    # View freelancer profile detail
+    path('freelancer/<int:id>/', views.freelancer_profile_detail, name='freelancer_profile_detail'),
 ]
